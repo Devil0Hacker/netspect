d8ac8557156cbd86b9ad69a2f7f98223af18132a
Devil0Hacker <sonyanshu415@gmail.com>
2026-01-14 20:59:34 +0530

    Delete core directory

--- core/core/core/core/installer.sh
+++
@@ -1,8 +0,0 @@
-#!/data/data/com.termux/files/usr/bin/bash
-clear
-pkg update -y
-pkg install -y python git nmap whois curl
-pip install holehe
-chmod +x netspect.py
-ln -sf $PWD/netspect.py $PREFIX/bin/netspect
-echo "Installed! Run using: netspect"
